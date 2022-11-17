cd38443147f4ac50504d6990f9c62406a848b48d
Jayhun24 <jayhunshrfkhan19@gmail.com>
2022-11-17 15:02:12 +0400

    add about , home

---
+++ src/pages/About/index.tsx
@@ -0,0 +1,14 @@
+import PageContainer from "../../components/PageContainer";
+
+const About = () => {
+    return (
+        <>
+            <PageContainer>
+                <div style={{border:'5px solid black'}}>
+                    bu ise contentdir
+                </div>
+            </PageContainer>
+        </>
+    )
+}
+export default About;
